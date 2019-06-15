dd92c033e41b8634a7ad5a36f36a556fc8f39347
Taylor Watson <taylorwatsonb@gmail.com>
2019-06-15 04:49:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,7 +12,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(age >= 13 && age <= 19){
+  if(age >= 13 && age <= 19) {
   return "You are a teenager!";
 } else if (age <= 12) {
   return "You are a kid";
